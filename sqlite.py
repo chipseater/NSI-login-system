4b76768dbbd9bcea097ed90fe7e982b7fb47afe2
chipseater <jeanpauldemalet@yahoo.fr>
2022-11-14 11:16:30 +0000

    played with db

--- sqlite.py
+++ sqlite.py
@@ -1,3 +1,16 @@
 import sqlite3
 
 con = sqlite3.connect('test.db')
+
+cursor = con.cursor()
+
+# cursor.execute("""
+#     CREATE TABLE employees (
+#         first text,
+#         last text,
+#         pay integer
+#     )
+# """)
+
+con.commit()
+con.close()
