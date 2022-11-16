9e3b048d8bec72093e915d5526f9eb507683b661
chipseater <jeanpauldemalet@yahoo.fr>
2022-11-16 20:46:01 +0100

    [Client] tidied up fetch function

--- client/js/fetch.js
+++ client/js/fetch.js
@@ -1,22 +1,31 @@
-function fetchToElement(element, route, verb) {
-  fetch(`http://localhost:8000${route}`, {
-    method: verb,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    "Access-Control-Allow-Origin": "*",
-    cache: "default",
-  })
-    .then((res) => {
-        res.json().then(res => {
-            element.innerHTML = res.text
-            console.log(res)
-        })
+function fetchRoute(route, verb) {
+  return new Promise((resolve, rejetct) => {
+    fetch(`http://localhost:5000${route}`, {
+      method: verb,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      'Access-Control-Allow-Origin': '*',
+      mode: 'cors',
+      cache: 'default',
     })
-    .catch((err) => {
-      element.innerHTML = err;
-    });
+      .then((res) => {
+        res
+          .json()
+          .then((res) => resolve(res))
+          .catch((err) => rejetct(err))
+      })
+      .catch((err) => {
+        element.innerHTML = err
+      })
+  })
 }
 
-const element = document.getElementById("hello-world");
-fetchToElement(element, "/", "GET");
+const element = document.getElementById('hello-world')
+fetchRoute('/', 'GET')
+  .then((res) => {
+    element.innerHTML = res.text
+  })
+  .catch((err) => {
+    console.error(err)
+  })
