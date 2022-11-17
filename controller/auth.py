ed10c6a61982baa97921905785b6c6a0b3c29326
chipseater <jeanpauldemalet@yahoo.fr>
2022-11-17 10:20:45 +0000

    [sqlite] Hello World!

--- controller/auth.py
+++ controller/auth.py
@@ -1,3 +1,29 @@
-from flask import Flask
+import sqlite3
 
 
+def dbFunc(func):
+    def inner(self, *args, **kwargs):
+        cursor = self.conn.cursor()
+        func(self, cursor, *args, **kwargs)
+        self.conn.commit()
+    return inner
+
+
+class AuthManager:
+    def __init__(self):
+        self.conn = sqlite3.connect('database/auth.db')
+    
+
+    @dbFunc
+    def helloWorld(self, cursor):
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            begin_date text,
+            end_date text
+        ); """)
+
+
+authmanager = AuthManager()
+authmanager.helloWorld()
